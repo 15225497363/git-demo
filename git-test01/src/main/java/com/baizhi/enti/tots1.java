e9c52d6c9618843089e7074ed4698f14c6511557
wyb <1793031607@qq.com>
2018-10-19 18:00:54 +0800

    这是第一次提交（只有一个类和main方法)

--- git-test01/src/main/java/com/baizhi/enti/tots1.java
+++ git-test01/src/main/java/com/baizhi/enti/tots1.java
@@ -1,10 +1,11 @@
 package com.baizhi.enti;
 
+
 /**
  * Created by 王云博 on 2018/10/19.
  */
 public class tots1 {
     public static void main(String[] args) {
-
+        System.out.println("hahah");
     }
 }
